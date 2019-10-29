396a9332e4afe9526a5b900e50fc711ce821c5f3
huangxing <447994518@qq.com>
2019-10-29 21:50:15 +0800

    test

--- src/test/java/test/lucene/LuceneTest.java
+++ src/test/java/test/lucene/LuceneTest.java
@@ -3,13 +3,15 @@ package test.lucene;
 import junit.framework.TestCase;
 import org.apache.commons.io.FileUtils;
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.*;
 import org.apache.lucene.index.*;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
+import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.*;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
+import org.wltea.analyzer.lucene.IKAnalyzer;
 
 import java.io.File;
 import java.io.IOException;
@@ -23,7 +25,7 @@ public class LuceneTest extends TestCase {
     //Directory directory = new RAMDirectory();
 
     //指定一个标准分析器，对文档内容进行分析
-    Analyzer analyzer = new StandardAnalyzer();
+    Analyzer analyzer = new IKAnalyzer();
 
     //创建indexwriterCofig对象
     //第一个参数： Lucene的版本信息，可以选择对应的lucene版本也可以使用LATEST
@@ -77,9 +79,10 @@ public class LuceneTest extends TestCase {
     indexWriter.close();
   }
 
+  protected String indexDirectory="./data/lucene/index";
   public void testMatchAllDocsQuery() throws Exception {
     //创建一个Directory对象，指定索引库存放的路径
-    Directory directory = FSDirectory.open(new File("./data/lucene/index"));
+    Directory directory = FSDirectory.open(new File(indexDirectory));
     //创建IndexReader对象，需要指定Directory对象
     IndexReader indexReader = DirectoryReader.open(directory);
     //创建Indexsearcher对象，需要指定IndexReader对象
@@ -119,14 +122,14 @@ public class LuceneTest extends TestCase {
   //搜索索引
   public void testSearchIndex() throws IOException{
     //创建一个Directory对象，指定索引库存放的路径
-    Directory directory = FSDirectory.open(new File("./data/lucene/index"));
+    Directory directory = FSDirectory.open(new File(indexDirectory));
     //创建IndexReader对象，需要指定Directory对象
     IndexReader indexReader = DirectoryReader.open(directory);
     //创建Indexsearcher对象，需要指定IndexReader对象
     IndexSearcher indexSearcher = new IndexSearcher(indexReader);
     //创建一个TermQuery（精准查询）对象，指定查询的域与查询的关键词
     //创建查询
-    Query query = new TermQuery(new Term("fileName", "LuceneTest"));
+    Query query = new TermQuery(new Term("fileName", "lucene"));
     //执行查询
     //第一个参数是查询对象，第二个参数是查询结果返回的最大值
     TopDocs topDocs = indexSearcher.search(query, 10);
@@ -156,7 +159,7 @@ public class LuceneTest extends TestCase {
   //数值范围查询
   public void testNumericRangeQuery() throws Exception {
     //创建一个Directory对象，指定索引库存放的路径
-    Directory directory = FSDirectory.open(new File("E:\\programme\\test"));
+    Directory directory = FSDirectory.open(new File(indexDirectory));
     //创建IndexReader对象，需要指定Directory对象
     IndexReader indexReader = DirectoryReader.open(directory);
     //创建Indexsearcher对象，需要指定IndexReader对象
@@ -169,7 +172,7 @@ public class LuceneTest extends TestCase {
     //3.最大值
     //4.是否包含最小值
     //5.是否包含最大值
-    Query query = NumericRangeQuery.newLongRange("fileSize", 41L, 2055L, true, true);
+    Query query = NumericRangeQuery.newLongRange("fileSize", 390L, 399L, true, true);
     //执行查询
 
     //第一个参数是查询对象，第二个参数是查询结果返回的最大值
@@ -199,4 +202,182 @@ public class LuceneTest extends TestCase {
     indexReader.close();
   }
 
+  //组合条件查询
+  public void testBooleanQuery() throws Exception {
+    //创建一个Directory对象，指定索引库存放的路径
+    Directory directory = FSDirectory.open(new File(indexDirectory));
+    //创建IndexReader对象，需要指定Directory对象
+    IndexReader indexReader = DirectoryReader.open(directory);
+    //创建Indexsearcher对象，需要指定IndexReader对象
+    IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+
+    //创建一个布尔查询对象
+    BooleanQuery query = new BooleanQuery();
+    //创建第一个查询条件
+    Query query1 = new TermQuery(new Term("fileName", "apache"));
+    Query query2 = new TermQuery(new Term("fileName", "lucene"));
+    //组合查询条件
+    query.add(query1, BooleanClause.Occur.SHOULD);
+    query.add(query2, BooleanClause.Occur.SHOULD);
+    //执行查询
+
+    //第一个参数是查询对象，第二个参数是查询结果返回的最大值
+    TopDocs topDocs = indexSearcher.search(query, 10);
+
+    //查询结果的总条数
+    System.out.println("查询结果的总条数："+ topDocs.totalHits);
+    //遍历查询结果
+    //topDocs.scoreDocs存储了document对象的id
+    //ScoreDoc[] scoreDocs = topDocs.scoreDocs;
+    for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+      //scoreDoc.doc属性就是document对象的id
+      //int doc = scoreDoc.doc;
+      //根据document的id找到document对象
+      Document document = indexSearcher.doc(scoreDoc.doc);
+      //文件名称
+      System.out.println(document.get("fileName"));
+      //文件内容
+      System.out.println(document.get("fileContent"));
+      //文件大小
+      System.out.println(document.get("fileSize"));
+      //文件路径
+      System.out.println(document.get("filePath"));
+      System.out.println("----------------------------------");
+    }
+    //关闭indexreader对象
+    indexReader.close();
+  }
+
+  public void testQueryParser() throws Exception {
+    //创建一个Directory对象，指定索引库存放的路径
+    Directory directory = FSDirectory.open(new File(indexDirectory));
+    //创建IndexReader对象，需要指定Directory对象
+    IndexReader indexReader = DirectoryReader.open(directory);
+    //创建Indexsearcher对象，需要指定IndexReader对象
+    IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+
+    //创建queryparser对象
+    //第一个参数默认搜索的域
+    //第二个参数就是分析器对象
+    QueryParser queryParser = new QueryParser("fileName", new IKAnalyzer());
+    //使用默认的域,这里用的是语法，下面会详细讲解一下
+//    Query query = queryParser.parse("lucene");
+    //不使用默认的域，可以自己指定域
+    Query query = queryParser.parse("fileContent:Version");
+    //执行查询
+
+
+    //第一个参数是查询对象，第二个参数是查询结果返回的最大值
+    TopDocs topDocs = indexSearcher.search(query, 10);
+
+    //查询结果的总条数
+    System.out.println("查询结果的总条数："+ topDocs.totalHits);
+    //遍历查询结果
+    //topDocs.scoreDocs存储了document对象的id
+    //ScoreDoc[] scoreDocs = topDocs.scoreDocs;
+    for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+      //scoreDoc.doc属性就是document对象的id
+      //int doc = scoreDoc.doc;
+      //根据document的id找到document对象
+      Document document = indexSearcher.doc(scoreDoc.doc);
+      //文件名称
+      System.out.println(document.get("fileName"));
+      //文件内容
+      System.out.println(document.get("fileContent"));
+      //文件大小
+      System.out.println(document.get("fileSize"));
+      //文件路径
+      System.out.println(document.get("filePath"));
+      System.out.println("----------------------------------");
+    }
+    //关闭indexreader对象
+    indexReader.close();
+  }
+
+  public void testMultiFiledQueryParser() throws Exception {
+    //创建一个Directory对象，指定索引库存放的路径
+    Directory directory = FSDirectory.open(new File(indexDirectory));
+    //创建IndexReader对象，需要指定Directory对象
+    IndexReader indexReader = DirectoryReader.open(directory);
+    //创建Indexsearcher对象，需要指定IndexReader对象
+    IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+
+    //可以指定默认搜索的域是多个
+    String[] fields = {"fileName", "fileContent"};
+    //创建一个MulitFiledQueryParser对象
+    MultiFieldQueryParser queryParser = new MultiFieldQueryParser(fields, new IKAnalyzer());
+    Query query = queryParser.parse("apache");
+    System.out.println(query);
+    //执行查询
+
+
+    //第一个参数是查询对象，第二个参数是查询结果返回的最大值
+    TopDocs topDocs = indexSearcher.search(query, 10);
+
+    //查询结果的总条数
+    System.out.println("查询结果的总条数："+ topDocs.totalHits);
+    //遍历查询结果
+    //topDocs.scoreDocs存储了document对象的id
+    //ScoreDoc[] scoreDocs = topDocs.scoreDocs;
+    for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+      //scoreDoc.doc属性就是document对象的id
+      //int doc = scoreDoc.doc;
+      //根据document的id找到document对象
+      Document document = indexSearcher.doc(scoreDoc.doc);
+      //文件名称
+      System.out.println(document.get("fileName"));
+      //文件内容
+      System.out.println(document.get("fileContent"));
+      //文件大小
+      System.out.println(document.get("fileSize"));
+      //文件路径
+      System.out.println(document.get("filePath"));
+      System.out.println("----------------------------------");
+    }
+    //关闭indexreader对象
+    indexReader.close();
+  }
+
+  //删除全部索引
+  public void testDeleteAllIndex() throws Exception {
+    Directory directory = FSDirectory.open(new File(indexDirectory));
+    Analyzer analyzer = new IKAnalyzer();
+    IndexWriterConfig config = new IndexWriterConfig(Version.LATEST, analyzer);
+    IndexWriter indexWriter = new IndexWriter(directory, config);
+    //删除全部索引
+    indexWriter.deleteAll();
+    //关闭indexwriter
+    indexWriter.close();
+  }
+
+  //根据查询条件删除索引
+  public void testDeleteIndexByQuery() throws Exception {
+    Directory directory = FSDirectory.open(new File(indexDirectory));
+    Analyzer analyzer = new IKAnalyzer();
+    IndexWriterConfig config = new IndexWriterConfig(Version.LATEST, analyzer);
+    IndexWriter indexWriter = new IndexWriter(directory, config);
+    //创建一个查询条件
+    Query query = new TermQuery(new Term("fileName", "apache"));
+    //根据查询条件删除
+    indexWriter.deleteDocuments(query);
+    //关闭indexwriter
+    indexWriter.close();
+  }
+
+  //修改索引库
+  public void testUpdateIndex() throws Exception {
+    Directory directory = FSDirectory.open(new File(indexDirectory));
+    Analyzer analyzer = new IKAnalyzer();
+    IndexWriterConfig config = new IndexWriterConfig(Version.LATEST, analyzer);
+    IndexWriter indexWriter = new IndexWriter(directory, config);
+    //创建一个Document对象
+    Document document = new Document();
+    //向document对象中添加域。
+    //不同的document可以有不同的域，同一个document可以有相同的域。
+    document.add(new TextField("fileName", "要更新的文档", Field.Store.YES));
+    document.add(new TextField("fileContent", "简介 Lucene 是一个基于 Java 的全文信息检索工具包。", Field.Store.YES));
+    indexWriter.updateDocument(new Term("fileName", "lucene"), document);
+    //关闭indexWriter
+    indexWriter.close();
+  }
 }
